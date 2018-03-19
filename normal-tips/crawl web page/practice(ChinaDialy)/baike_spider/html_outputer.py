c968b99f4e1f3b1eb5df4ee6b658d54dae42fb0a
Xuyining <oxuyining@gmail.com>
2018-03-19 09:16:33 +0800

    ChinaDialy for parctice with notes

---
+++ normal-tips/crawl web page/practice(ChinaDialy)/baike_spider/html_outputer.py
@@ -0,0 +1,49 @@
+import os
+import csv
+
+class HtmlOutputer(object):
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    def output_html(self):
+        fout = open('output.html', 'w')
+        fout.write("<html>")
+        fout.write("<body>")
+        fout.write("<table>")
+
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data['url'])
+            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            fout.write("</tr>")
+
+        fout.write("</table>")
+        fout.write("</body>")
+        fout.write("</html>")
+
+        fout.close()
+
+
+    def output_execl(self, data, file, page):
+        import codecs
+        import sys
+        reload(sys)
+        sys.setdefaultencoding('utf8')
+
+        headers = [u'id', u'pubtime', u'title', u'des', u'content', u'img', u'url', u'time',
+                   u'address', u'type', u'gname', u'nwound', u'nkill', u'total']
+        headerflag = True
+        if os.path.exists(file):
+            headerflag = False
+        with codecs.open(file, 'ab+', 'utf8') as f:
+            f_csv = csv.DictWriter(f, headers)
+            if headerflag:
+                f_csv.writeheader()
+            f_csv.writerows(data)
+        print 'page %d save to \'%s\' sucessfully!' % (page, file)
--- normal-tips/crawl web page/practice(ChinaDialy)/practice.py
+++ normal-tips/crawl web page/practice(ChinaDialy)/practice.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+后期可以修改的！
+
+1、针对网站的域名进行爬取，中国日报网的域名上的所有网页应该都是相同格式的。可以用正则表达式
+    eg: http://world.chinadaily.com.cn/2017-04/05/content_28798021.htm
+    http://world.chinadaily.com.cn/   对应的格式
+    eg: http://cnews.chinadaily.com.cn/2017-04/04/content_28791640.htm
+    http://cnews.chinadaily.com.cn/   对应的格式
+
+2、注意，try可以用在调试，但是要记得打印出错误，同时兼顾单步调试，像爬虫这样的代码，用try except 来
+注意爬取网站不行就continue是很好的做法。
+
+3、to_join = [content_p[k].text for k in range(len(content_p))] 语句简单明了，通俗好用
+
+4、本代码加入了pyltp进行相关的段落短语提取，但是出现了不同的错误，基本指向segment fault，
+    目前原因尚未查明。
+
+5、曾经遇见过一个问题，在爬取网页的时候遇见了动态页面，翻页时页面链接是一样的，猜测用到了Ajax。
+    有软件可以解决这个问题。(Phantomjs软件)，一个基于webkit内核的无头浏览器，即没有UI界面，
+    即它就是一个浏览器，只是其内的点击、翻页等人为相关操作需要程序设计实现。
+
+"""
 
 from baike_spider import url_manager, html_downloader, html_parser, html_outputer
 import fact_triple_extraction1chen
@@ -12,14 +36,14 @@ class SpiderMain(object):
 
     def craw(self, root_url):
         count = 1
-        for i in range(30):#21
-            start_url = root_url.replace("offset=20", "offset=%d" % count)
+        for i in range(149):
+            start_url = root_url.replace("page=1", "page=%d" % count)
             html = self.downloader.download(start_url)
             # print html
             new_urls = self.parser.parse(start_url, html)
             self.urls.add_new_urls(new_urls)
             print 'craw %d %s' % (count, start_url)
-            count = count + 20
+            count = count + 1
 
         count = 1
         while self.urls.has_new_url():
@@ -78,7 +102,7 @@ class SpiderMain(object):
 
             datas = []
             datas.append(news)
-            self.outputer.output_execl(datas, "SputnikNews.csv", count)
+            self.outputer.output_execl(datas, "ChinaDialy.csv", count)
             count = count + 1
             EventInfoExtract.release_module()
 
@@ -103,6 +127,6 @@ class SpiderMain(object):
 
 
 if __name__=="__main__":
-    root_url = "http://sputniknews.cn/services/search/getmore/?search_area=all&query%5B%5D=%E6%81%90%E6%80%96%E8%A2%AD%E5%87%BB&limit=500&offset=20&sort=date&interval=period"
+    root_url = "http://search.chinadaily.com.cn/cn.jsp?searchText=%E6%81%90%E6%80%96%E8%A2%AD%E5%87%BB&page=1&strAuthor=&strFromdate=&strTodate=&usedate="
     obj_spider = SpiderMain()
     obj_spider.craw(root_url)
