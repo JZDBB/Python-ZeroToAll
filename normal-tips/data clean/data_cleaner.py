062e2f8973bb5a762b3235b8fa6f0ba455c85d36
Xuyining <oxuyining@gmail.com>
2018-05-01 15:59:37 +0800

    data clean 循环显示ok

--- normal-tips/data clean/data_cleaner.py
+++ normal-tips/data clean/data_cleaner.py
@@ -1,5 +1,6 @@
 """
-
+1、输入
+2、多行文本输入
 
 """
 # from ulits import Data
@@ -38,14 +39,15 @@ def read_csv(path):
 
 
 class CleanerFrame(wx.Frame):
-    def __init__(self):
+    def __init__(self, cn_path, en_path, id):
         wx.Frame.__init__(self, None, -1, "data clean",
-                          size=(1000, 800))
-        self.row = 0
-        self.col = 0
-        self.id_cn, self.list_cn, self.dict_cn = read_csv('Chinese.csv')
-        self.id_en, self.list_en, self.dict_en = read_csv('English.csv')
+                          size=(600, 400))
+
+        self.id_cn, self.list_cn, self.dict_cn = read_csv(cn_path)
+        self.id_en, self.list_en, self.dict_en = read_csv(en_path)
         self.id_cn[0] = 'enventid'
+        self.row = self.list_cn.index(str(id))
+        self.col = 0
         key = self.list_cn[self.row]
         init_en = self.dict_en[key]
         # init_en = init_en[id_cn[self.col + 1]]
@@ -57,16 +59,16 @@ class CleanerFrame(wx.Frame):
         str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
         panel = wx.Panel(self, -1)
         sizer = wx.BoxSizer(wx.VERTICAL)
-        upButton = wx.Button(panel, -1, '上一条', pos=(20, 160))
-        downButton = wx.Button(panel, -1, '下一条', pos=(150, 160))
+        upButton = wx.Button(panel, -1, '上一条', pos=(20, 250))
+        downButton = wx.Button(panel, -1, '下一条', pos=(150, 250))
         downButton.SetDefault()
-        saveButton = wx.Button(panel, -1, '保存', pos=(250, 160))
+        saveButton = wx.Button(panel, -1, '保存', pos=(250, 250))
         self.staticText_id = wx.StaticText(panel, -1, label=str_id, pos=(0, 10))
         staticText_en = wx.StaticText(panel, -1, label='EN', pos = (20, 30))
-        self.enText = wx.TextCtrl(panel, value=init_en, pos=(20, 50), size=(160, 30))
-        staticText_cn = wx.StaticText(panel, -1, label='CN', pos=(20, 90))
-        self.cnText = wx.TextCtrl(panel, value=init_cn, pos=(20, 110), size=(160, 30))
-        self.staticReform = wx.StaticText(panel, -1, label='', pos=(20, 200))
+        self.enText = wx.TextCtrl(panel, value=init_en, pos=(20, 50), size=(500, 80), style = wx.TE_MULTILINE|wx.HSCROLL)
+        staticText_cn = wx.StaticText(panel, -1, label='CN', pos=(20, 140))
+        self.cnText = wx.TextCtrl(panel, value=init_cn, pos=(20, 160), size=(500, 80), style = wx.TE_MULTILINE|wx.HSCROLL)
+        self.staticReform = wx.StaticText(panel, -1, label='', pos=(20, 300))
 
         sizer.Add(self.staticText_id, 0)
         sizer.Add(staticText_en, 0)
@@ -86,33 +88,40 @@ class CleanerFrame(wx.Frame):
         init_cn[self.id_cn[self.col + 1]] = value
         self.col -= 1
         if self.col < 0:
-            self.col = len(self.id_cn) - 1
+            self.col = len(self.id_cn) - 2
             self.row -= 1
             if self.row < 0:
-                self.staticReform.SetLabel('没有上一条')
-                return
-
+                self.row = len(self.list_cn) - 1
         key = self.list_cn[self.row]
         init_en = self.dict_en[key]
         # init_en = init_en[id_cn[self.col + 1]]
         while not init_en.__contains__(self.id_cn[self.col + 1]):
-            self.col += 1
+            self.col -= 1
+            if self.col < 0:
+                self.col = len(self.id_cn) - 2
+                self.row -= 1
+                if self.row < 0:
+                    self.row = len(self.list_cn) - 1
         init_en = init_en[self.id_cn[self.col + 1]]
         init_cn = self.dict_cn[self.list_cn[self.row]]
         init_cn = init_cn[self.id_cn[self.col + 1]]
         while init_cn == '':
             self.col -= 1
             if self.col < 0:
-                self.col = len(self.id_cn) - 1
+                self.col = len(self.id_cn) - 2
                 self.row -= 1
                 if self.row < 0:
-                    self.staticReform.SetLabel('没有上一条')
-                    return
+                    self.row = len(self.list_cn) - 1
             key = self.list_cn[self.row]
             init_en = self.dict_en[key]
             # init_en = init_en[id_cn[self.col + 1]]
             while not init_en.__contains__(self.id_cn[self.col + 1]):
-                self.col += 1
+                self.col -= 1
+                if self.col < 0:
+                    self.col = len(self.id_cn) - 2
+                    self.row -= 1
+                    if self.row < 0:
+                        self.row = len(self.list_cn) - 1
             init_en = init_en[self.id_cn[self.col + 1]]
             init_cn = self.dict_cn[self.list_cn[self.row]]
             init_cn = init_cn[self.id_cn[self.col + 1]]
@@ -132,9 +141,8 @@ class CleanerFrame(wx.Frame):
         if self.col >= (len(self.id_cn)-1):
             self.col = 0
             self.row += 1
-            if self.row > len(self.list_cn):
-                self.staticReform.SetLabel('没有下一条')
-                return
+            if self.row >= len(self.list_cn):
+                self.row = 0
         key = self.list_cn[self.row]
         init_en = self.dict_en[key]
         # init_en = init_en[id_cn[self.col + 1]]
@@ -148,9 +156,8 @@ class CleanerFrame(wx.Frame):
             if self.col >= (len(self.id_cn) - 1):
                 self.col = 0
                 self.row += 1
-                if self.row > len(self.list_cn):
-                    self.staticReform.SetLabel('没有下一条')
-                    return
+                if self.row >= len(self.list_cn):
+                    self.row = 0
 
             key = self.list_cn[self.row]
             init_en = self.dict_en[key]
@@ -184,7 +191,8 @@ class CleanerFrame(wx.Frame):
 
 
 if __name__ == '__main__':
+    
     app = wx.App()
-    frame = CleanerFrame()
+    frame = CleanerFrame('Chinese.csv', 'English.csv', 197001010002)
     frame.Show()
     app.MainLoop()
