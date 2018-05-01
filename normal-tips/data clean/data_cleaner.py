33bf2882d4b47879374ac515dc8595db136e3d72
Xuyining <oxuyining@gmail.com>
2018-05-01 14:52:59 +0800

    data clean 终版1.0上

--- normal-tips/data clean/data_cleaner.py
+++ normal-tips/data clean/data_cleaner.py
@@ -6,10 +6,14 @@
 import wx
 import csv
 
-def write_csv(path, list):
+def write_csv(path, list_stream):
     csvFile = open(path, "w")
-    writer = csv.writer(csvFile)
-    writer.writerow(list)
+    for stream in list_stream:
+        content = ','.join(stream) + '\n'
+        csvFile.write(content)
+    # writer = csv.writer(csvFile)
+    # for stream in list_stream:
+    #     writer.writerow(stream)
     csvFile.close()
 
 
@@ -41,7 +45,7 @@ class CleanerFrame(wx.Frame):
         self.col = 0
         self.id_cn, self.list_cn, self.dict_cn = read_csv('Chinese.csv')
         self.id_en, self.list_en, self.dict_en = read_csv('English.csv')
-        str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
+        self.id_cn[0] = 'enventid'
         key = self.list_cn[self.row]
         init_en = self.dict_en[key]
         # init_en = init_en[id_cn[self.col + 1]]
@@ -50,6 +54,7 @@ class CleanerFrame(wx.Frame):
         init_en = init_en[self.id_cn[self.col + 1]]
         init_cn = self.dict_cn[self.list_cn[self.row]]
         init_cn = init_cn[self.id_cn[self.col + 1]]
+        str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
         panel = wx.Panel(self, -1)
         sizer = wx.BoxSizer(wx.VERTICAL)
         upButton = wx.Button(panel, -1, '上一条', pos=(20, 160))
@@ -61,18 +66,21 @@ class CleanerFrame(wx.Frame):
         self.enText = wx.TextCtrl(panel, value=init_en, pos=(20, 50), size=(160, 30))
         staticText_cn = wx.StaticText(panel, -1, label='CN', pos=(20, 90))
         self.cnText = wx.TextCtrl(panel, value=init_cn, pos=(20, 110), size=(160, 30))
+        self.staticReform = wx.StaticText(panel, -1, label='', pos=(20, 200))
 
         sizer.Add(self.staticText_id, 0)
         sizer.Add(staticText_en, 0)
         sizer.Add(self.enText, 0)
         sizer.Add(staticText_cn, 0)
         sizer.Add(self.cnText, 0)
+        sizer.Add(self.staticReform, 0)
 
         upButton.Bind(wx.EVT_BUTTON, self.onClickUp)
         downButton.Bind(wx.EVT_BUTTON, self.onClickDown)
         saveButton.Bind(wx.EVT_BUTTON, self.onClickSave)
 
     def onClickUp(self, event):
+        self.staticReform.SetLabel('')
         value = self.cnText.GetValue()
         init_cn = self.dict_cn[self.list_cn[self.row]]
         init_cn[self.id_cn[self.col + 1]] = value
@@ -80,8 +88,10 @@ class CleanerFrame(wx.Frame):
         if self.col < 0:
             self.col = len(self.id_cn) - 1
             self.row -= 1
-        str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
-        self.staticText_id.SetLabel(str_id)
+            if self.row < 0:
+                self.staticReform.SetLabel('没有上一条')
+                return
+
         key = self.list_cn[self.row]
         init_en = self.dict_en[key]
         # init_en = init_en[id_cn[self.col + 1]]
@@ -95,8 +105,9 @@ class CleanerFrame(wx.Frame):
             if self.col < 0:
                 self.col = len(self.id_cn) - 1
                 self.row -= 1
-            str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
-            self.staticText_id.SetLabel(str_id)
+                if self.row < 0:
+                    self.staticReform.SetLabel('没有上一条')
+                    return
             key = self.list_cn[self.row]
             init_en = self.dict_en[key]
             # init_en = init_en[id_cn[self.col + 1]]
@@ -106,11 +117,14 @@ class CleanerFrame(wx.Frame):
             init_cn = self.dict_cn[self.list_cn[self.row]]
             init_cn = init_cn[self.id_cn[self.col + 1]]
 
+        str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
+        self.staticText_id.SetLabel(str_id)
         self.enText.SetValue(init_en)
         self.cnText.SetValue(init_cn)
 
 
     def onClickDown(self, event):
+        self.staticReform.SetLabel('')
         value = self.cnText.GetValue()
         init_cn = self.dict_cn[self.list_cn[self.row]]
         init_cn[self.id_cn[self.col + 1]] = value
@@ -118,8 +132,9 @@ class CleanerFrame(wx.Frame):
         if self.col >= (len(self.id_cn)-1):
             self.col = 0
             self.row += 1
-        str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
-        self.staticText_id.SetLabel(str_id)
+            if self.row > len(self.list_cn):
+                self.staticReform.SetLabel('没有下一条')
+                return
         key = self.list_cn[self.row]
         init_en = self.dict_en[key]
         # init_en = init_en[id_cn[self.col + 1]]
@@ -133,24 +148,36 @@ class CleanerFrame(wx.Frame):
             if self.col >= (len(self.id_cn) - 1):
                 self.col = 0
                 self.row += 1
-            str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
-            self.staticText_id.SetLabel(str_id)
+                if self.row > len(self.list_cn):
+                    self.staticReform.SetLabel('没有下一条')
+                    return
+
             key = self.list_cn[self.row]
             init_en = self.dict_en[key]
-            # init_en = init_en[id_cn[self.col + 1]]
             while not init_en.__contains__(self.id_cn[self.col + 1]):
                 self.col += 1
             init_en = init_en[self.id_cn[self.col + 1]]
             init_cn = self.dict_cn[self.list_cn[self.row]]
             init_cn = init_cn[self.id_cn[self.col + 1]]
 
+        str_id = 'eventid = ' + self.list_cn[self.row] + '，id = ' + self.id_cn[self.col + 1]
+        self.staticText_id.SetLabel(str_id)
         self.enText.SetValue(init_en)
         self.cnText.SetValue(init_cn)
 
     def onClickSave(self, event):
-        pass
+        list_csv = []
+        list_csv.append(self.id_cn)
+        for i in range(len(self.list_cn)):
+            list_line = []
+            list_line.append(self.list_cn[i])
+            for j in range(len(self.id_cn) - 1):
+                cn = self.dict_cn[self.list_cn[i]]
+                list_line.append(cn[self.id_cn[j + 1]])
+            list_csv.append(list_line)
 
-
+        write_csv('new.csv', list_stream=list_csv)
+        self.staticReform.SetLabel('保存成功')
 
 
 
