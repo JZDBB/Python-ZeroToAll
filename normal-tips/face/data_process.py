c0c98565415ad718927b3eba83dd07bef3eef2fe
XuYining <oxuyining@gmail.com>
2018-10-10 12:17:45 +0800

    file read need debug and split picture need ajust

--- normal-tips/face/data_process.py
+++ normal-tips/face/data_process.py
@@ -1,15 +1,13 @@
 import cv2
-import face_recognition
 
-def detectFaces(image_name):
-    img = cv2.imread(image_name)
-    face_cascade = cv2.CascadeClassifier("/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml")
+def detectFaces(img):
+    face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
     if img.ndim == 3:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     else:
         gray = img #if语句：如果img维度为3，说明不是灰度图，先转化为灰度图gray，如果不为3，也就是2，原图就是灰度图
 
-    faces = face_cascade.detectMultiScale(gray, 1.2, 5)#1.3和5是特征的最小、最大检测窗口，它改变检测结果也会改变
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)#1.3和5是特征的最小、最大检测窗口，它改变检测结果也会改变
     result = []
     for (x,y,width,height) in faces:
         result.append((x,y,x+width,y+height))
@@ -38,22 +36,26 @@ def main():
     # line = f.readline()
     # while line:
     #     line = f.readline()
-    #     filename, label = line.split('g')
+    #     try:
+    #         filename, label = line.split('g')
+    #     except
     #     filename = filename + 'g'
     #     dict_label[filename] = label
     #
     # for file in list_name:
-    #
-
-
-
-    img = cv2.imread('Img/img_align_celeba/img_align_celeba/000001.jpg', 0)
+    img = cv2.imread("Img/img_align_celeba/img_align_celeba/000001.jpg")
     coordinate = detectFaces(img)
     x1, y1, x2, y2 = coordinate[0]
-    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255,0), 5)
+    # print(x1, y1, x2, y2)
+    # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    # cv2.imshow('', img)
+    # cv2.waitKey(0)
+    img = img[x1:x2, y1:y2]
+    # img_save = cv2.resize(img, (256, 256), interpolation=cv2.INTER_CUBIC)
     cv2.imshow('', img)
     cv2.waitKey(0)
-
+    # cv2.imwrite('1.jpg', img_save)
+    print('ok')
 
 if __name__ == '__main__':
     main()
